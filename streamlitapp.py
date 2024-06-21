495a2bd15a9a0123426d821c047b7a8b44f0ef16
rpmish99 <31197163+rpmish99@users.noreply.github.com>
2024-06-21 11:51:20 -0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.write(""" # My First App """)
+
+# Load data
+url = "https://raw.githubusercontent.com/plotly/datasets/master/diabetes-vid.csv"
+df = pd.read_csv(url)
+st.line_chart(df)
+
 
