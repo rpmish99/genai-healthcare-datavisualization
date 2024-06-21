958b14168ff03716f35bae80257231599583b3b6
rpmish99 <31197163+rpmish99@users.noreply.github.com>
2024-06-21 12:17:34 -0700

    Update streamlitapp.py

--- src/streamlitapp.py
+++ src/streamlitapp.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import pandas as pd
+import streamlit_option_menu
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+  selected = option_menu(
+    menu_title = "Main Menu",
+    options = ["Home","Projects","Contact"],
+    icons = ["house","book","envelope"],
+    menu_icon = "cast",
+    default_index = 0,
 
 st.write(""" # My First App """)
 
